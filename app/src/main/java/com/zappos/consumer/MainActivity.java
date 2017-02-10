d8c1451522884765d7114d60493125f2ffb354c2
NareshPoola <naresh.p@xcubelabs.com>
2017-02-11 01:53:06 +0530

    Intial Commit Implemented Functionalities

---
+++ app/src/main/java/com/zappos/consumer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zappos.consumer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * Created by Ramya on 9/2/17.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
